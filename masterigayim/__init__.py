c8976c0ece13d7e7f6dd6fcf79b446e15b8122d5
Marko Ristin <marko.ristin@gmail.com>
2023-02-20 22:00:44 +0100

    Release 0.0.2
    * Fix rendering precondition (#2)

--- masterigayim/__init__.py
+++ masterigayim/__init__.py
@@ -1,6 +1,6 @@
 """Have fun cleaning virtual windows."""
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __author__ = "Marko Ristin"
 __license__ = "License :: OSI Approved :: MIT License"
 __status__ = "Production/Stable"
